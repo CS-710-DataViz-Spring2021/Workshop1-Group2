6e2d2a068eb37d5bf84ea59c9c3ecfebeb2a996d
tristntran <tristntran@gmail.com>
2021-02-03 17:50:59 -0800

    Improved data manipulation
    bins and such

--- src/R/EloWorkshop.R
+++ src/R/EloWorkshop.R
@@ -1,12 +1,30 @@
 packages<-c("ggplot2","tibble","dplyr")
 install.packages(setdiff(packages, rownames(installed.packages())))
-library(ggplot)
+library(ggplot2)
 library(tibble)
 library(dplyr)
+#downlads the file and reads filters out all away games to avoid duplicate data and forced symmetry
 d <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv")
 d<- tibble(d)
 d <- filter(d,game_location=="H")
+
+#reformats the data selecting only relevant columns performs some transformations
+#creates point_diff column home team minus away team points
+#creates the elo_diff column. Home team minus away team elo
+#Creates a column for the predicted result
+#recodes the game result in binary variables with 1 being a win
+#creates a column that evaluates whether or not elo accurately predicts the the result of the game
+#puts the data into bins for the sake of graphing
+bin_width = 25
+
 results <- d %>% select(game_id,elo_i,opp_elo_i,pts,opp_pts,game_result)%>%
   mutate(point_diff=pts-opp_pts)%>%
   mutate(elo_diff=elo_i-opp_elo_i) %>%
-  mutate(game_result=recode(game_result,`L`=0,`W`=1))
+  mutate(game_result=recode(game_result,`L`=0,`W`=1))%>%
+  mutate(pred_result=elo_diff>0)%>%
+  mutate(pred_accurate= pred_result==game_result)%>%
+  mutate(elo_bin=cut(elo_diff,seq(min(elo_diff)%/%bin_width*bin_width-bin_width,max(elo_diff)%/%bin_width*bin_width+bin_width,by=bin_width),right=FALSE))
+
+bin_summary <- results %>% 
+  group_by(elo_bin,pred_accurate)%>%
+  summarise(Count=n())
