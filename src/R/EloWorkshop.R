8045c47ccfb5df65ff35e7e81c7c20b0e95fc40b
tristntran <tristntran@gmail.com>
2021-02-04 20:56:28 -0800

    got the graphs working

--- src/R/EloWorkshop.R
+++ src/R/EloWorkshop.R
@@ -15,16 +15,29 @@ d <- filter(d,game_location=="H")
 #recodes the game result in binary variables with 1 being a win
 #creates a column that evaluates whether or not elo accurately predicts the the result of the game
 #puts the data into bins for the sake of graphing
-bin_width = 25
+bin_width = 50
 
-results <- d %>% select(game_id,elo_i,opp_elo_i,pts,opp_pts,game_result)%>%
-  mutate(point_diff=pts-opp_pts)%>%
+results <- d %>% select(game_id,elo_i,opp_elo_i,pts,opp_pts,game_result) %>%
+  mutate(point_diff=pts-opp_pts) %>%
   mutate(elo_diff=elo_i-opp_elo_i) %>%
-  mutate(game_result=recode(game_result,`L`=0,`W`=1))%>%
-  mutate(pred_result=elo_diff>0)%>%
-  mutate(pred_accurate= pred_result==game_result)%>%
-  mutate(elo_bin=cut(elo_diff,seq(min(elo_diff)%/%bin_width*bin_width-bin_width,max(elo_diff)%/%bin_width*bin_width+bin_width,by=bin_width),right=FALSE))
+  mutate(game_result=recode(game_result,`L`=0,`W`=1)) %>%
+  mutate(pred_result=elo_diff>0) %>%
+  mutate(pred_accurate= pred_result==game_result) %>%
+  mutate(elo_bin=cut(elo_diff,seq(min(elo_diff)%/%bin_width*bin_width-bin_width,
+                                  max(elo_diff)%/%bin_width*bin_width+bin_width,by=bin_width),
+                     right=FALSE))
+
 
 bin_summary <- results %>% 
-  group_by(elo_bin,pred_accurate)%>%
-  summarise(Count=n())
+  group_by(elo_bin,pred_accurate) %>%
+  summarise(count=n())
+
+predict_plot <- results%>% ggplot +
+  aes(x=elo_bin,fill=elo_bin)+
+  geom_histogram()
+
+pt_diff_plot<- results %>% ggplot +
+  aes(x=elo_diff,y=point_diff,color=elo_i)+
+  scale_color_steps()+
+  geom_point()+
+  labs(title= "Home Team Point Difference vs Elo Difference")
